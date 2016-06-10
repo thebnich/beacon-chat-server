5b37cc8e7d15676f405e4956285209ed635c427f
Brian Nicholson <thebnich@gmail.com>
2016-06-10 14:13:02 -0700

    Update directory structure

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var dateFormat = require('dateformat');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'));
 
 var messages = {};
 
